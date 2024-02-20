f5252e71f93106f4c109a6bda112b5e0c1e62b5e
13551057653 <zhangkaichao@zhangkahaodeMBP.lan>
2024-02-20 16:13:19 +0800

    提交代码

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <pig-table msg="Welcome to Your Vue.js App"></pig-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import pigTable from '../src/package/pig-table/index.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    pigTable
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 </style>
---
+++ src/package/pig-table/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <button>我是测试按钮</button>
+    </div>
+</template>
+<script>
+export default {
+    name: "pig-table", //组件名
+};
+</script>
+<style scoped>
+button {
+    width: 100px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+    text-align: center;
+}
+</style>
