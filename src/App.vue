04d578a6a0ff4d40172dec0b734516938d3b49a6
13551057653 <zhangkaichao@zhangkahaodeMBP.lan>
2024-02-20 17:44:54 +0800

    提交代码

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <pig-table></pig-table>
+    <pig-table :tableData="tableData"></pig-table>
   </div>
 </template>
 
@@ -11,6 +11,27 @@ export default {
   name: 'App',
   components: {
     pigTable
+  },
+  data() {
+    return {
+      tableData: [{
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄'
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄'
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄'
+          }]
+    }
   }
 }
 </script>
--- src/package/pig-table/index.vue
+++ src/package/pig-table/index.vue
@@ -17,27 +17,14 @@
 <script>
 export default {
     name: 'HelloWorld',
+    props: {
+        tableData:{
+            type: Array,
+            default: () => []
+        }
+    },
     date() {
         return {
-            tableData: [
-                {
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                    date: '2016-05-04',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1517 弄'
-                }, {
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1519 弄'
-                }, {
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1516 弄'
-                }
-            ],
             multipleSelection: []
         }
     },
